0154451c770f1fe9ca899f811ebc506babddef3e
kalil0321 <92564081+kalil0321@users.noreply.github.com>
2022-06-08 16:28:48 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Website of kalil0321!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <Header title="This is the website of kalil0321." />
         <p className="description">
-          I'm currently a Bachelor CS student at <a href="epfl.ch"> EPFL</a>.
+          I am currently a Bachelor CS student at <a href="epfl.ch">EPFL</a>.
         </p>
       </main>
 
