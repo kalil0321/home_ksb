133466ae290f50af898fbb9defd52bd9a9e3d2e6
kalil0321 <92564081+kalil0321@users.noreply.github.com>
2022-06-08 16:43:37 +0200

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,13 @@ export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
-        Made with <img src="/netliheart.svg" alt="Netlify Logo" className={styles.logo} /> for you
+        Made with <img src="/netliheart.svg" alt="Netlify Logo" className={styles.logo} /> for you.
+        <div>Contacts 
+          <div><a href= "https://instagram.com/kalil0321">
+            <img href="https://commons.wikimedia.org/wiki/File:Instagram_logo.svg" alt="Instagram Logo" />
+            </a> 
+          </div>
+        </div>
       </footer>
     </>
   )
