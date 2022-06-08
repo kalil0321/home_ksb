f91b2fa5666bc7ef2518cd697927caa8403d78fc
kalil0321 <92564081+kalil0321@users.noreply.github.com>
2022-06-08 16:25:08 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,26 +11,6 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
-
-      <Footer />
-    </div>
-  )
-}
-
-export default function Home2() {
-  return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
         <Header title="This is the website of kalil0321." />
         <p className="description">
           I'm currently a Bachelor CS student at <a href="epfl.ch"> EPFL</a>.
